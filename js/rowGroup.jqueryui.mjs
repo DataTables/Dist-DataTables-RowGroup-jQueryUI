d84a2344954797b9e58673b0b761bab449a2bd23
Allan Jardine <allan@datatables.net>
2022-10-10 19:50:21 +0000

    Add support for mjs files (ES Modules)

---
+++ js/rowGroup.jqueryui.mjs
@@ -0,0 +1,13 @@
+
+/*! jQuery UI styling wrapper for RowGroup
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-ju';
+import DataTable from 'datatables.net-rowgroup';
+
+
+
+
+export default DataTable;
