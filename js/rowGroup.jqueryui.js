445f10af6fbf2b0be53bcb779273144b5408bcf1
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 16:41:57 +0000

    d2b1dea28a80633f7f8a6b4d320b8dc04bf409d0 Release 1.3.0
    Sync to source repo @d2b1dea28a80633f7f8a6b4d320b8dc04bf409d0

--- js/rowGroup.jqueryui.js
+++ js/rowGroup.jqueryui.js
@@ -1,4 +1,3 @@
-
 /*! jQuery UI styling wrapper for RowGroup
  * © SpryMedia Ltd - datatables.net/license
  */
--- js/rowGroup.jqueryui.mjs
+++ js/rowGroup.jqueryui.mjs
@@ -1,11 +1,10 @@
-
 /*! jQuery UI styling wrapper for RowGroup
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-ju';
-import DataTable from 'datatables.net-rowgroup';
+import 'datatables.net-rowgroup';
 
 
 
