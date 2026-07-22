7dc6b658a029417c92d45231545f07fe31cd8824
dtbuild <dtbuild@rigil>
2026-07-22 14:46:38 +0000

    f8eae909be69bd3f000913ac3f9672a8d765f734 Release 2.0.0 ready
    Sync to source repo @f8eae909be69bd3f000913ac3f9672a8d765f734

--- js/rowGroup.jqueryui.min.mjs
+++ js/rowGroup.jqueryui.min.mjs
@@ -1,4 +1,4 @@
-/*! RowGroup jQuery UI styling 2.0.0-beta.1 for DataTables
+/*! RowGroup jQuery UI styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-jqui";import RowGroup from"datatables.net-rowgroup";export default DataTable;
